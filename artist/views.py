bb0ce2a4ecc862204aae40b8e00085cb3125ffdb
tazin407 <tazintasnim34@gmail.com>
2024-05-23 21:08:00 +0600

    is_verified

--- artist/views.py
+++ artist/views.py
@@ -84,7 +84,7 @@ def activate(request, uidb64, token):
         user= None
         
     if user is not None and account_activation_token.check_token(user, token):
-        user.email_is_verified=True 
+        user.is_verified=True 
         user.save()
         # messages.success(request,'Your account has been verified successfully')
         return HttpResponse('Your account has been verified successfully. You may proceed to log in.')
