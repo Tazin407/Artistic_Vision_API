824ed41c20436777ab37f4d483f864a99ebcafde
tazin407 <tazintasnim34@gmail.com>
2024-05-24 14:53:11 +0600

    commit

--- art/serializers.py
+++ art/serializers.py
@@ -3,12 +3,10 @@ from django.contrib.auth import authenticate, get_user_model, get_user
 from .import models
 
 class ShowArts(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
-    artist_name = serializers.CharField(source='artist.name', read_only=True)
+    
     class Meta:
         model= models.Art
-        fields= fields = ['id', 'title', 'image', 'description','likes', 'artist', 'artist_name']
-        
+        fields='__all__'
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model= models.Like
